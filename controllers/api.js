151e33889a9d00f759949a8676050dcbf0908274
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-28 12:02:41 +0200

    change valid url testing method

--- controllers/api.js
+++ controllers/api.js
@@ -6,11 +6,16 @@ exports.getHello = (req, res, next) => {
 };
 
 exports.postNew = async (req, res, next) => {
-  const httpRegex = /^(http|https)(:\/\/)/;
   const url = req.body.url;
   const urlCode = shortid.generate();
 
-  if (!httpRegex.test(url)) {
+  function urlIsValid(url) {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+      url
+    );
+  }
+
+  if (!urlIsValid(url)) {
     res.status(401).json({ error: 'Invalid URL' });
   } else {
     try {
