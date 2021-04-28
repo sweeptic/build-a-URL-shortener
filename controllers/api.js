7b7705f7674eb1da3a7284a02952fc580d3c5fda
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-28 14:41:40 +0200

    correct dns lookup parameter

--- controllers/api.js
+++ controllers/api.js
@@ -1,5 +1,5 @@
 const shortid = require('shortid');
-const URL = require('../models/url');
+const Mongo_URL = require('../models/url');
 const dns = require('dns');
 
 exports.getHello = (req, res, next) => {
@@ -7,22 +7,30 @@ exports.getHello = (req, res, next) => {
 };
 
 exports.postNew = async (req, res, next) => {
-  const REPLACE_REGEX = /^https?:\/\//i;
+  // const REPLACE_REGEX = /^https?:\/\//i;
   const url = req.body.url;
   const urlCode = shortid.generate();
 
-  const domain = await url.replace(REPLACE_REGEX, '');
+  // const domain = await url.replace(REPLACE_REGEX, '');
+
+  let domain = new URL(url);
+  domain = domain.hostname;
 
   if (!req.body.url.includes('http')) {
     res.json({ error: 'invalid URL' });
     return;
   } else {
+    console.log(domain);
     dns.lookup(domain, async (err, add, fam) => {
       // If the URL does not exist, return expected error
-      if (err) return res.json({ error: 'invalid URL' });
+
+      if (err) {
+        console.log(err);
+        return res.json({ error: 'invalid URL' });
+      }
 
       try {
-        let findOne = await URL.findOne({ original_url: url });
+        let findOne = await Mongo_URL.findOne({ original_url: url });
 
         if (findOne) {
           res.json({
@@ -30,7 +38,7 @@ exports.postNew = async (req, res, next) => {
             short_url: findOne.short_url,
           });
         } else {
-          findOne = new URL({
+          findOne = new Mongo_URL({
             original_url: url,
             short_url: urlCode,
           });
@@ -51,7 +59,7 @@ exports.postNew = async (req, res, next) => {
 
 exports.getShortURL = async (req, res, next) => {
   try {
-    const urlParams = await URL.findOne({
+    const urlParams = await Mongo_URL.findOne({
       short_url: req.params.short_url,
     });
 
